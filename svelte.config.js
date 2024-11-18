77ef3e4d494d41cb8f17051bf7a9288b8570f9df
Victor Rodriguez <awtGerry@gmail.com>
2024-11-17 23:59:55 -0600

    vercel cfg

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
-
-const dev = process.env.NODE_ENV === 'development';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,9 +7,6 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-			base: dev ? '' : '/rifa_gera'
-		},
 	},
 };
 
