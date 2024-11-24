42823d4b4a361d7659ed76c111fef8f7dec96b0f
Victor Rodriguez <awtGerry@gmail.com>
2024-11-24 17:53:31 -0600

    winners

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -21,6 +21,11 @@
       <img src="/bateria2.jpg" alt="Bateria T-Fal 21 piezas" class="rounded-xl w-32 sm:w-40 mt-4">
     </div>
   </div>
+  <!-- Winner -->
+  <div class="flex items-center justify-center mt-2">
+    <p class="text-slate-500 mt-2 text-sm text-center">El ganador de la bateria es: <span class="text-red-500">21</span></p>
+  </div>
+
 
   <h4 class="text-lg text-lime-700 mt-4 text-center">Segundo lugar: Tenis Nike AF1 1:1 (talla a elegir por el ganador)</h4>
   <div class="flex items-center justify-center mt-2">
@@ -30,7 +35,12 @@
       <img src="/nike2.jpg" alt="Tenis Nike AF1 1:1" class="rounded-xl w-32 sm:w-40 mt-4">
     </div>
   </div>
+  <!-- Winner -->
+  <div class="flex items-center justify-center mt-2">
+    <p class="text-slate-500 mt-2 text-sm text-center">El ganador de los tenis es: <span class="text-red-500">51</span></p>
+  </div>
 
   <!-- Info Text -->
-  <p class="text-slate-500 mt-2 text-sm text-center">La rifa se llevara a cabo al terminar la venta de numeros</p>
+  <!-- <p class="text-slate-500 mt-2 text-sm text-center">La rifa se llevara a cabo al terminar la venta de numeros</p> -->
+  <h4 class="text-lg text-lime-700 mt-4 text-center">Gracias a todos por su apoyo!</h4>
 </section>
